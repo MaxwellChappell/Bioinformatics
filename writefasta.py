59f4f73e0e07b24eccaa7f5a17f75c75610952e9
MaxwellChappell <60639983+MaxwellChappell@users.noreply.github.com>
2020-09-16 00:31:58 -0500

    added comments

--- writefasta.py
+++ writefasta.py
@@ -1,5 +1,9 @@
-
-
+'''
+This takes a list of names and sequences and writes them to a file in
+fasta format
+@param file_name the name of the file it will write to
+@param cases the list of lists with the amino acid sequences and gene names
+'''
 def write_fasta(file_name, cases):
     file = open(file_name, "w+")
     for case in cases:
